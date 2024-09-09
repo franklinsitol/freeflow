89d188b53afd07ac740079f04c2f9eaeae0b29c1
franklinsitol <franklin.toledo@gmail.com>
2024-09-09 15:16:19 -0300

    Update listar_posts.php

--- listar_posts.php
+++ listar_posts.php
@@ -1,19 +1,4 @@
 <?php
-$diretorio = 'posts/';
-$arquivos = glob($diretorio . '*.json');
-$posts = [];
-
-// Coleta todos os posts
-foreach ($arquivos as $arquivo) {
-    $post = json_decode(file_get_contents($arquivo), true);
-    $posts[] = $post;
-}
-
-// Retorna os posts como JSON
-header('Content-Type: application/json');
-echo json_encode($posts);
-?>
-<?php
 // Caminho para o diretório
 $postsDir = __DIR__ . '/posts/';
 $postsFile = $postsDir . 'posts.json';
@@ -25,5 +10,3 @@ if (file_exists($postsFile)) {
     echo json_encode([]);
 }
 ?>
-
-
