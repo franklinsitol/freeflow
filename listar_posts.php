ff38f45943f0064358f8ac91154dd9750013d1ef
franklinsitol <franklin.toledo@gmail.com>
2024-09-09 15:13:11 -0300

    Update listar_posts.php

--- listar_posts.php
+++ listar_posts.php
@@ -13,4 +13,17 @@ foreach ($arquivos as $arquivo) {
 header('Content-Type: application/json');
 echo json_encode($posts);
 ?>
+<?php
+// Caminho para o diretório
+$postsDir = __DIR__ . '/posts/';
+$postsFile = $postsDir . 'posts.json';
+
+if (file_exists($postsFile)) {
+    header('Content-Type: application/json');
+    echo file_get_contents($postsFile);
+} else {
+    echo json_encode([]);
+}
+?>
+
 
