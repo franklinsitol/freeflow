6e1aa707ed259c977209da03e3080fe06ed07f0a
franklinsitol <franklin.toledo@gmail.com>
2024-09-09 15:13:34 -0300

    Create delete_post.php

---
+++ delete_post.php
@@ -0,0 +1,28 @@
+<?php
+// Caminho para os diretórios
+$postsDir = __DIR__ . '/posts/';
+$uploadsDir = __DIR__ . '/uploads/';
+
+// Lê a entrada JSON
+$data = json_decode(file_get_contents('php://input'), true);
+$id = $data['id'] ?? null;
+
+if ($id) {
+    $postsFile = $postsDir . 'posts.json';
+
+    if (file_exists($postsFile)) {
+        $posts = json_decode(file_get_contents($postsFile), true);
+        $posts = array_filter($posts, function ($post) use ($id) {
+            return $post['id'] !== $id;
+        });
+
+        file_put_contents($postsFile, json_encode($posts, JSON_PRETTY_PRINT));
+
+        echo 'Postagem excluída com sucesso!';
+    } else {
+        echo 'Arquivo de postagens não encontrado!';
+    }
+} else {
+    echo 'ID não fornecido!';
+}
+?>
